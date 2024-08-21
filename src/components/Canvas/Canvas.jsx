4af07bb9872cea75df051acd40f4411e4bf27c2e
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-08-21 20:07:55 +0300

    add Brush

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,11 +1,22 @@
-import "./Canvas.scss"
+import { useEffect } from "react";
+import "./Canvas.scss";
+import { observer } from "mobx-react-lite";
+import canvasState from "../../store/canvasState";
+import { useRef } from "react";
+import toolState from "../../store/toolState";
+import Brush from "../../tools/Brush";
 
-const Canvas = () => {
+const Canvas = observer(() => {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current);
+    toolState.setTool(new Brush(canvasRef.current));
+  }, []);
   return (
     <div className="canvas">
-      <canvas width={600}height={400}></canvas>
+      <canvas width={600} height={400} ref={canvasRef}></canvas>
     </div>
   );
-};
-
+});
 export default Canvas;
--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,12 +1,18 @@
+import canvasState from "../../store/canvasState";
+import toolState from "../../store/toolState";
+import Brush from "../../tools/Brush";
 import "./Toolbar.scss";
 
 const Toolbar = () => {
   return (
     <div className="toolbar">
-      <button className="toolbar__btn toolbar__btn_brush"></button>
-      <button className="toolbar__btn  toolbar__btn_color"></button>
+      <button
+        className="toolbar__btn toolbar__btn_brush"
+        onClick={() => toolState.setTool(new Brush(canvasState.canvas))}
+      ></button>
+      <button className="toolbar__btn  toolbar__btn_rect"></button>
       <button className="toolbar__btn  toolbar__btn_figure"></button>
-      <button className="toolbar__btn  toolbar__btn_eraser"></button>
+      <button className="toolbar__btn  toolbar__btn_eraser"></button> 
       <button className="toolbar__btn  toolbar__btn_line"></button>
       <input className="toolbar__color-input" type="color" />
       <button className="toolbar__btn  toolbar__btn_back"></button>
