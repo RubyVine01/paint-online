9d3bf487e3d64951265de21961086193651a3efd
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-08-23 00:48:14 +0300

    add socket

--- client/src/components/Canvas/Canvas.jsx
+++ client/src/components/Canvas/Canvas.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Canvas.scss";
 import { observer } from "mobx-react-lite";
 import canvasState from "../../store/canvasState";
@@ -8,31 +8,85 @@ import Brush from "../../tools/Brush";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useParams } from "react-router-dom";
 
 const Canvas = observer(() => {
   const canvasRef = useRef();
+  const usernameRef = useRef();
+  const [modal, setModal] = useState(true);
+  const [error, setError] = useState("");
+  const params = useParams();
 
   useEffect(() => {
     canvasState.setCanvas(canvasRef.current);
     toolState.setTool(new Brush(canvasRef.current));
   }, []);
 
+  useEffect(() => {
+    if (canvasState.username) {
+      const socket = new WebSocket("ws://localhost:5001");
+      canvasState.setSocket(socket);
+      canvasState.setSesstionId(params.id);
+
+      socket.onopen = () => {
+        console.log("Получилось подключиться");
+        socket.send(
+          JSON.stringify({
+            id: params.id,
+            username: canvasState.username,
+            method: "connection",
+          })
+        );
+        socket.onmessage = (event) => {
+          let msg = JSON.parce(event.data);
+          switch (msg.method) {
+            case "connection":
+              console.log(`Пользователь ${msg.username} присоединился`);
+              break;
+            case "draw":
+              drawHendler(msg)
+              break;
+          }
+        };
+      };
+    }
+  }, [canvasState.username]);
+
+const drawHendler =(msg) => {
+
+}
+
   const mouseDownHandler = () => {
     canvasState.pushToUndo(canvasRef.current.toDataURL());
   };
 
-  const connectHandler = () => {};
+  const connectHandler = () => {
+    const username = usernameRef.current.value.trim();
+    if (!username) {
+      setError("Имя не может быть пустым!");
+      return;
+    }
+
+    canvasState.setUsername(usernameRef.current.value);
+    setModal(false);
+    setError("");
+  };
+
   return (
     <div className="canvas">
-      <Modal show={true} onHide={() => {}}>
+      <Modal show={modal} onHide={() => {}}>
         <Modal.Header closeButton>
           <Modal.Title>Введите ваше имя</Modal.Title>
         </Modal.Header>
         <Form.Control
+          ref={usernameRef}
           type="text"
           placeholder="Ваше Имя"
           style={{ margin: "0.8rem 1rem", width: "auto" }}
         />
+        {error && (
+          <p style={{ color: "red", marginLeft: "1.375rem" }}>{error}</p>
+        )}
         <Modal.Footer>
           <Button variant="secondary" onClick={() => connectHandler()}>
             Войти
--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
-const express =  require('express')
-const app = express()
-const WSServer = require('express-ws')(app)
+const express = require("express");
+const app = express();
+const WSServer = require("express-ws")(app);
+const aWss = WSServer.getWss();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-app.ws('/', (ws, req) => {
-    console.log('ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО ');
+app.ws("/", (ws, req) => {
+  console.log("ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО ");
 
-    ws.send('Ты успешно подключился')
-    ws.on('message', (msg) => {
-        console.log(msg);
-    })
-})
+  ws.send("Ты успешно подключился");
+  ws.on("message", (msg) => {
+    msg = JSON.parse(msg);
+    switch (msg.method) {
+      case "connection":
+        connectionHendler(ws, msg);
+        break;
+        case "draw":
+        connectionHendler(ws, msg);
+        break;
+    }
+  });
+});
 
-app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
- 
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+
+const connectionHendler = (ws, msg) => {
+  ws.id = msg.id;
+  broadcastConnection(ws, msg);
+};
+
+const broadcastConnection = (ws, msg) => {
+  aWss.clients.forEach((client) => {
+    if (client.id === msg.id) {
+      client.send(JSON.stringify(msg));
+    }
+  });
+};
